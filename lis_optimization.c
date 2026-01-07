3aa509e6772b02061e3b201d27f048729029c4bc
shadythegiant <hellgatesdesigner2010@gmail.com>
2026-01-07 22:36:29 +0100

    oh my leg

--- lis_optimization.c
+++ lis_optimization.c
@@ -1,2 +1,20 @@
 #include "push_swap.h"
 
+static int *list_to_array(t_stack *stack , int size )
+{
+	int		*arr; 
+	int		i; 
+
+	i = 0; 
+	arr = malloc(sizeof(int) * size); 
+	if(!arr)
+		return NULL; 
+	while(stack)
+	{
+		arr[i] = stack->value; 
+		stack = stack->next; 
+		i++; 
+	}	
+	return (arr); 
+}
+
